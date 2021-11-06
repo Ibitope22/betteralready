764245ff1821ca49e54f39cd7b3f0d4a490e6f48
ibitope22 <ibitopefatoki@hotmail.com>
2021-11-06 22:21:58 +0400

    fix

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ const express= require('express');
 const app=express();
 
 dotenv.config({path:"../server/config/config.env"});
+
+app.listen(procees.env.PORT, ()=>{
+    console.log(`app running on port ${process.env.PORT}`)
+});
