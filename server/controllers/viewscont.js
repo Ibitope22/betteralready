13c4b6edcc16785fcf71524beac9b430c3957dcf
ibitope22 <ibitopefatoki@hotmail.com>
2021-12-11 16:55:36 +0400

    fixes

--- server/controllers/viewscont.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = mongoose("mongoose")
-
-exports.getLoginForm = (req, res) => {
-  res.status(200).render("login", {
-    title: "Login",
-  });
-};
--- server/routes/viewroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = mongoose("mongoose")
-
-const viewscont = require("../controllers/viewscont");
-
-const router = express.Router();
-
-router.get("/login", viewscont.getLoginForm);
